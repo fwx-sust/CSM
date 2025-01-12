28611f4d419b8f93d1e2546fc3881e59638909b6
fwx-sust <12211951@mail.sustech.edu.cn>
2025-01-12 18:21:50 +0800

    Add files via upload

--- Proj/driver.m
+++ Proj/driver.m
@@ -2,13 +2,39 @@
 %1*1矩形实验
 clear all; clc;
 
+%自定义
 E=2e11; %Young's modulus
 v=0.3; %Poisson's ratio
-q=1e5; %N/m^2
 
-%D(x)————specify either plane strain or plane stress
-question_def = 2; %1 for plane strain, 2 for plane stress
-D = stiffnessD(question_def, E, v);
+%body force: 自定义
+f=@(x,y,dof) (dof==1)*0+...
+    (dof==2)*0;
+Da=E/(v+1)/(2*v-1);
+f=@(x,y,dof) (dof==1)*(-Da)*( 2*(v-1)*(y^2-y)-v*(2*x-1)*(2*y-1)+(2*v-1)*(x^2-x)+0.5*(2*v-1)*(2*x-1)*(2*y-1) ) +...
+    (dof==2)*(-Da)*( 2*(v-1)*(x^2-x)-v*(2*y-1)*(2*x-1)+(2*v-1)*(y^2-y)+0.5*(2*v-1)*(2*y-1)*(2*x-1) ) ;
+%exact solution————以下是最简单的四边固支的位移————手输，画图时要用
+%位移理论值
+fux=@(x,y) x.*(x-1).*y.*(y-1); %点乘方便后续画图
+fuy=@(x,y) x.*(x-1).*y.*(y-1);
+%偏导理论值
+fux_x=@(x,y) (2.*x-1).*(y.^2-y);
+fux_y=@(x,y) (2.*y-1).*(x.^2-x);
+fuy_x=@(x,y) (2.*x-1).*(y.^2-y);
+fuy_y=@(x,y) (2.*y-1).*(x.^2-x);
+%应变理论值
+f_strain_xx=@(x,y) (2.*x-1).*(y.^2-y);
+f_strain_yy=@(x,y) (x.^2-x).*(2.*y-1);
+f_strain_xy=@(x,y) 0.5*( (x.^2-x).*(2.*y-1)+(2.*x-1).*(y.^2-y) );
+f_strain_zz=@(x,y) -v/E *( Da*( (v-1).*(2.*x-1).*(y.^2-y)-v.*(x.^2-x).*(2.*y-1) )+ Da*( -v.*(2.*x-1).*(y.^2-y)+(v-1).*(x.^2-x).*(2.*y-1) ) ); %only for plane stress
+%应力理论值
+f_stress_xx=@(x,y) Da*( (v-1).*(2.*x-1).*(y.^2-y)-v.*(x.^2-x).*(2.*y-1) );
+f_stress_yy=@(x,y) Da*( -v.*(2.*x-1).*(y.^2-y)+(v-1).*(x.^2-x).*(2.*y-1) );
+f_stress_xy=@(x,y) Da*(2*v-1)*0.5.*( (x.^2-x).*(2.*y-1)+(2.*x-1).*(y.^2-y) );
+f_stress_zz=@(x,y) v*( Da*( (v-1).*(2.*x-1).*(y.^2-y)-v.*(x.^2-x).*(2.*y-1) )+Da*( -v.*(2.*x-1).*(y.^2-y)+(v-1).*(x.^2-x).*(2.*y-1) ) ); %only for plane strain
+
+%D(x)————specify either plane strain or plane stress：自定义
+question_def = 1; %1 for plane strain, 2 for plane stress
+D0 = stiffnessD(question_def, E, v);
 
 % quadrature rule
 n_int_xi  = 3;
@@ -19,12 +45,16 @@ n_int     = n_int_xi * n_int_eta;
 % mesh generation——gmsh生成
 %n_np:total number of nodal points
 %n_el:total number of elements
+%改变网格可能需要改变读网格的方法，但若只是更改每条边的节点数，可以直接用这个方法
 [n_np,n_el,x_coor,y_coor,IEN,top_pos,bottom_pos,left_pos,right_pos] = try_to_read_mesh('rectangle_gmsh.m');
-n_en = 4;               % number of nodes in an element 四边形网格
+%top_pos等返回的是边界坐标，后面发现其实直接返回A会更好
+
+n_en = 4;  % number of nodes in an element 四边形网格
 n_ed = 2;  %两个自由度
 
 %求单元面积
-area=zeros(n_el);
+area=zeros(n_el,1);
+area_s=zeros(n_el,1);
 for i=1:n_el
     x1=x_coor( IEN(i,1) );
     y1=y_coor( IEN(i,1) );
@@ -35,64 +65,31 @@ for i=1:n_el
     x4=x_coor( IEN(i,4) );
     y4=y_coor( IEN(i,4) );
     area(i)=0.5 * abs( x1*y2 + x2*y3 + x3*y4 + x4*y1 - (y1*x2 + y2*x3 + y3*x4 + y4*x1) );
+    area_s(i)=sqrt(area(i));
 end
+h=max(area_s);
 
-%力边界条件: 可改
-f=@(x,y,dof) 0;%no body force
+%边界条件
 
-% =0 代表Dirichlet（设置位移g） Neumann: =1 代表应力边界条件（仅考虑自由边界，及应力为0的情况） =2代表设置力边界条件
-%可变
-boundary_conditions=[2,2,1,1,1,1,0,0]; %顺序为上下左右(各有两个方向） 下边界固定，左右边界自由，上边界受力
+%外载荷边界条件
+force_boundary=[0,0,0,0]; %==0代表没有均布载荷，==1代表有均布载荷（只考虑均布载荷）
+force_top_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
+force_bottom_f = @(x,y,dof) (dof==1)* 0+ (dof==2)* 0;
+force_left_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
+force_right_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
 
+% =0 代表Dirichlet（设置位移g） Neumann: =1 代表应力边界条件（设置应力）
+%自定义
+boundary_conditions=[0,0,0,0,0,0,0,0];
 %提供Dirichlet条件g————位移边界条件：可以改变
-% for i_bc=1:8 %四条边*2
-%     if boundary_conditions(i_bc) == 0
-%         if i_bc==1 || i_bc==2
-%             %(dof == 1: x) (dof == 2: y)
-%             disp_top_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
-%         else
-%             if  i_bc==3 || i_bc==4
-%                 disp_bottom_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
-%             else
-%                 if i_bc==5 || i_bc==6
-%                     disp_left_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
-%                 else
-%                     disp_right_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
-%                 end
-%             end
-%         end
-%     end
-% end
-
-%手动输入
+%手动输入————bc中哪个是0就改哪个，其余的不注释也可以，dof==1是x方向，dof==2是y方向
 disp_top_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
-%除了下边界，其余均是Not necessary for the question I defined
-disp_bottom_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
+disp_bottom_f = @(x,y,dof) (dof==1)* 0+ (dof==2)* 0;
 disp_left_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
 disp_right_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
 
-%提供Neumann边界条件h————应力边界条件：只考虑应力为0的情况，不可改
-% for i_bc=1:8
-%     if boundary_conditions(i_bc) == 1
-%         if i_bc==1
-%             %(dof == 1: x) (dof == 2: y)
-%             stress_top_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
-%         else
-%             if  i_bc==2
-%                 stress_bottom_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
-%             else
-%                 if i_bc==3
-%                     stress_left_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
-%                 else
-%                     stress_right_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
-%                 end
-%             end
-%         end
-%     end
-% end
-
-%手动输入，但该代码只考虑自由端，即应力为0
-%同理，除了左右边界，其余的not necessary
+%提供Neumann边界条件h————应力边界条件：可以改变
+%手动输入，————bc中哪个是1就改哪个，其余的不注释也可以，dof==1是x方向，dof==2是y方向
 stress_top_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
 stress_bottom_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
 stress_left_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
@@ -101,10 +98,9 @@ stress_right_f = @(x,y,dof) (dof==1)* 0 + (dof==2)* 0;
 % ID array——两个自由度
 ID = zeros(n_np,n_ed);
 counter = 0;
-A_for_q=[];
 for i=1:n_np
     %判断是否在边界上
-    flag=0; %=1 top; =2 bottom; =3 left; =4 right 和bc矩阵对应
+    flag=0;  %=1 top; =3 bottom; =5 left; 7 right 和bc矩阵对应
     a=x_coor(i);
     b=y_coor(i);
 
@@ -136,30 +132,21 @@ for i=1:n_np
     if flag==0
         counter=counter+1;
         ID(i,1)=counter;
-        A_for_q(counter) =0;
         counter=counter+1;
         ID(i,2)=counter;
-        A_for_q(counter) =0;
     else
         %在边界上，Neumann边界照常给P，Dirichlet直接默认0
-        if boundary_conditions(flag) ~=0
+        if boundary_conditions(flag) ~=0 %x方向
             counter=counter+1;
             ID(i,1)=counter;
         end
-        if boundary_conditions(flag+1) ~=0
+        if boundary_conditions(flag+1) ~=0 %y方向
             counter=counter+1;
             ID(i,2)=counter;
         end
-        %记录外载荷向量的位置
-        if boundary_conditions(flag+1) ==2
-            A_for_q(counter)=1; %y方向
-            A_for_q(counter-1) =0;
-        else
-            A_for_q(counter)=0;
-            A_for_q(counter-1)=0;
-        end
     end
 end
+
 n_eq = counter;
 LM=zeros(2,n_el,n_en);
 for i=1:n_el
@@ -169,10 +156,10 @@ for i=1:n_el
     end
 end
 
-
 % allocate the stiffness matrix and load vector
-K = spalloc(n_eq, n_eq, 9 * n_eq);
+K = spalloc(n_eq, n_eq,n_eq^2 -2);
 F = zeros(n_eq, 1);
+
 
 % loop over element to assembly the matrix and vector
 for ee = 1 : n_el
@@ -206,58 +193,126 @@ for ee = 1 : n_el
             Na_y = (-Na_xi * dx_deta + Na_eta * dx_dxi) / detJ;
 
             Ba=[Na_x, 0; 0, Na_y; Na_y, Na_x];
-            kappa1=D*Ba;
+            DB=D0*Ba;
+            % B1a=Na_x;B2a=Na_y;
+            % DB(1,1)=D(1,1)*B1a; DB(1,2)=D(1,2)*B2a; DB(2,1)=D(1,2)*B1a; DB(2,2)=D(2,2)*B2a; DB(3,1)=D(3,3)*B2a; DB(3,2)=D(3,3)*B1a;
 
             f_ele(2*aa-1) = f_ele(2*aa-1) + weight(ll) * detJ * f(x_l, y_l,1) * Na;
             f_ele(2*aa) = f_ele(2*aa) + weight(ll) * detJ * f(x_l, y_l,2) * Na;
-            for bb = 1 : aa
+
+            % modify F with the boundary data(Neumann条件:stress 和均布载荷）
+            a=x_coor( IEN(ee,aa) );
+            b=y_coor( IEN(ee,aa) );
+            %先判断bc，再判断位置能减少计算时间
+            if boundary_conditions(1)==1 || boundary_conditions(2)==1
+                for m=1:size(top_pos,1)
+                    if a== top_pos(m,1) && b==top_pos(m,2)
+                        f_ele(2*aa-1) = f_ele(2*aa-1) + weight(ll) * detJ * stress_top_f(a,b,1) * Na;
+                        f_ele(2*aa) = f_ele(2*aa) + weight(ll) * detJ * stress_top_f(a,b,2) * Na;
+                        if force_boundary(1) == 1   %top一般只受y方向上的均布载荷
+                            f_ele(2*aa-1) = f_ele(2*aa-1) + weight(ll)*detJ*force_top_f(a,b,1)*Na;
+                            f_ele(2*aa) = f_ele(2*aa) + weight(ll)*detJ*force_top_f(a,b,2)*Na;
+                        end
+                    end
+                end
+            end
+            if boundary_conditions(3)==1 || boundary_conditions(4)==1
+                for m=1:size(bottom_pos,1)
+                    if a== bottom_pos(m,1) && b==bottom_pos(m,2)
+                        f_ele(2*aa-1) = f_ele(2*aa-1) + weight(ll) * detJ * stress_bottom_f(a,b,1) * Na;
+                        f_ele(2*aa) = f_ele(2*aa) + weight(ll) * detJ * stress_bottom_f(a,b,2) * Na;
+                        if force_boundary(2) == 1
+                            f_ele(2*aa-1) = f_ele(2*aa-1) + weight(ll)*detJ*force_bottom_f(a,b,1)*Na;
+                            f_ele(2*aa) = f_ele(2*aa) + weight(ll)*detJ*force_bottom_f(a,b,2)*Na;
+                        end
+                    end
+                end
+            end
+            if boundary_conditions(5)==1 || boundary_conditions(6)==1
+                for m=1:size(left_pos,1)
+                    if a== left_pos(m,1) && b==left_pos(m,2)
+                        f_ele(2*aa-1) = f_ele(2*aa-1) + weight(ll) * detJ * stress_left_f(a,b,1) * Na;
+                        f_ele(2*aa) = f_ele(2*aa) + weight(ll) * detJ * stress_left_f(a,b,2) * Na;
+                        if force_boundary(3) == 1
+                            f_ele(2*aa-1) = f_ele(2*aa-1) + weight(ll)*detJ*force_left_f(a,b,1)*Na;
+                            f_ele(2*aa) = f_ele(2*aa) + weight(ll)*detJ*force_left_f(a,b,2)*Na;
+                        end
+                    end
+                end
+            end
+            if boundary_conditions(7)==1 || boundary_conditions(8)==1
+                for m=1:size(right_pos,1)
+                    if a== right_pos(m,1) && b==right_pos(m,2)
+                        f_ele(2*aa-1) = f_ele(2*aa-1) + weight(ll) * detJ * stress_right_f(a,b,1) * Na;
+                        f_ele(2*aa) = f_ele(2*aa) + weight(ll) * detJ * stress_right_f(a,b,2) * Na;
+                        if force_boundary(4) == 1
+                            f_ele(2*aa-1) = f_ele(2*aa-1) + weight(ll)*detJ*force_right_f(a,b,1)*Na;
+                            f_ele(2*aa) = f_ele(2*aa) + weight(ll)*detJ*force_right_f(a,b,2)*Na;
+                        end
+                    end
+                end
+            end
+
+            for bb = 1 : n_en
                 Nb = Quad(bb, xi(ll), eta(ll));
                 [Nb_xi, Nb_eta] = Quad_grad(bb, xi(ll), eta(ll));
                 Nb_x = (Nb_xi * dy_deta - Nb_eta * dy_dxi) / detJ;
                 Nb_y = (-Nb_xi * dx_deta + Nb_eta * dx_dxi) / detJ;
                 Bb=[Nb_x, 0; 0, Nb_y; Nb_y, Nb_x];
-                kappa=(Bb')*kappa1;
-                k_ele(2*aa-1,2*bb-1) = k_ele(2*aa-1,2*bb-1) + weight(ll) * detJ * kappa(1,1) * (Na_x * Nb_x + Na_y * Nb_y);
-                k_ele(2*aa-1,2*bb) = k_ele(2*aa-1,2*bb)+ weight(ll) * detJ * kappa(1,2) * (Na_x * Nb_x + Na_y * Nb_y);
-                k_ele(2*aa,2*bb-1) = k_ele(2*aa,2*bb-1)+ weight(ll) * detJ * kappa(2,1) * (Na_x * Nb_x + Na_y * Nb_y);
-                k_ele(2*aa,2*bb) = k_ele(2*aa,2*bb)+ weight(ll) * detJ * kappa(2,2) * (Na_x * Nb_x + Na_y * Nb_y);
+                BDB=(Bb')*DB;
+                % B1b=Nb_x; B2b=Nb_y;
+                % BDB(1,1)=B1b*DB(1,1)+B2b*DB(3,1); BDB(1,2)=B1b*DB(1,2)+B2b*DB(3,2); BDB(2,1)=B2b*DB(2,1)+B1b*DB(3,1); BDB(2,2)=B2b*DB(2,2)+B1b*DB(3,2);
+                k_ele(2*aa-1,2*bb-1) = k_ele(2*aa-1,2*bb-1) + weight(ll) * detJ * BDB(1,1) ;
+                k_ele(2*aa-1,2*bb) = k_ele(2*aa-1,2*bb)+ weight(ll) * detJ * BDB(1,2) ;
+                k_ele(2*aa,2*bb-1) = k_ele(2*aa,2*bb-1)+ weight(ll) * detJ * BDB(2,1) ;
+                k_ele(2*aa,2*bb) = k_ele(2*aa,2*bb)+ weight(ll) * detJ * BDB(2,2) ;
             end
         end
     end
 
     %K and F
-
     for aa = 1 : n_en
         for i=1:2
             PP = LM(i,ee, aa);
             if PP > 0
                 F(PP) = F(PP) + f_ele(2*(aa-1)+i);
-                for bb = 1 : n_en
-                    QQ = LM(i,ee, bb);
-                    if QQ > 0
-                        K(PP, QQ) = K(PP, QQ) + k_ele(2*(aa-1)+i, 2*(bb-1)+i);
-                    else %QQ=0
-                        % modify F with the boundary data(Dirichlet条件)
-                        a=x_coor( IEN(ee,bb) );
-                        b=y_coor( IEN(ee,bb) );
-                        for j=1:size(top_pos,1)
-                            if a== top_pos(j,1) && b==top_pos(j,2)
-                                F(PP)= F(PP) - k_ele(2*(aa-1)+i, 2*(bb-1)+i)*disp_top_f(a,b,i);
+                for j=1:2
+                    for bb = 1 : n_en
+                        QQ = LM(j,ee, bb);
+                        if QQ > 0
+                            K(PP, QQ) = K(PP, QQ) + k_ele(2*(aa-1)+i, 2*(bb-1)+j);
+                        else %QQ=0
+                            % modify F with the boundary data(Dirichlet条件)
+                            a=x_coor( IEN(ee,bb) );
+                            b=y_coor( IEN(ee,bb) );
+                            %先判断bc,在判断位置能简化计算
+                            if boundary_conditions(j)==0
+                                for m=1:size(top_pos,1)
+                                    if a== top_pos(m,1) && b==top_pos(m,2)
+                                        F(PP)= F(PP) - k_ele(2*(aa-1)+i, 2*(bb-1)+j)*disp_top_f(a,b,j);
+                                    end
+                                end
                             end
-                        end
-                        for j=1:size(bottom_pos,1)
-                            if a== bottom_pos(j,1) && b==bottom_pos(j,2)
-                                F(PP)= F(PP) - k_ele(2*(aa-1)+i, 2*(bb-1)+i)*disp_bottom_f(a,b,i);
+                            if boundary_conditions(2+j)==0
+                                for m=1:size(bottom_pos,1)
+                                    if a== bottom_pos(m,1) && b==bottom_pos(m,2)
+                                        F(PP)= F(PP) - k_ele(2*(aa-1)+i, 2*(bb-1)+j)*disp_bottom_f(a,b,j);
+                                    end
+                                end
                             end
-                        end
-                        for j=1:size(left_pos,1)
-                            if a== left_pos(j,1) && b==left_pos(j,2)
-                                F(PP)= F(PP) - k_ele(2*(aa-1)+i, 2*(bb-1)+i)*disp_left_f(a,b,i);
+                            if boundary_conditions(4+j)==0
+                                for m=1:size(left_pos,1)
+                                    if a== left_pos(m,1) && b==left_pos(m,2)
+                                        F(PP)= F(PP) - k_ele(2*(aa-1)+i, 2*(bb-1)+j)*disp_left_f(a,b,j);
+                                    end
+                                end
                             end
-                        end
-                        for j=1:size(right_pos,1)
-                            if a== right_pos(j,1) && b==right_pos(j,2)
-                                F(PP)= F(PP) - k_ele(2*(aa-1)+i, 2*(bb-1)+i)*disp_right_f(a,b,i);
+                            if boundary_conditions(6+j)==0
+                                for m=1:size(right_pos,1)
+                                    if a== right_pos(m,1) && b==right_pos(m,2)
+                                        F(PP)= F(PP) - k_ele(2*(aa-1)+i, 2*(bb-1)+j)*disp_right_f(a,b,j);
+                                    end
+                                end
                             end
                         end
                     end
@@ -265,15 +320,7 @@ for ee = 1 : n_el
             end
         end
     end
-    %在此处考虑添加的力边界条件，修改F
-    for i_bc=1:4
-        if boundary_conditions(i_bc) == 2
-            %但是我不会
-        end
-    end
 end
-%如果只在端点处加力边界条件，在此处修改F
-F=q*A_for_q';
 
 % solve the stiffness matrix
 %displacement
@@ -291,24 +338,33 @@ for ii = 1 : n_np
             % modify disp with the g data.
             a=x_coor( ii );
             b=y_coor( ii );
-            for j=1:size(top_pos,1)
-                if a== top_pos(j,1) && b==top_pos(j,2)
-                    disp(ii,i) = disp_top_f(a,b,i);
+            %判断在哪条边上
+            if boundary_conditions(i)==0
+                for j=1:size(top_pos,1)
+                    if a== top_pos(j,1) && b==top_pos(j,2)
+                        disp(ii,i) = disp_top_f(a,b,i);
+                    end
                 end
             end
-            for j=1:size(bottom_pos,1)
-                if a== bottom_pos(j,1) && b==bottom_pos(j,2)
-                    disp(ii,i) = disp_bottom_f(a,b,i);
+            if boundary_conditions(2+i)==0
+                for j=1:size(bottom_pos,1)
+                    if a== bottom_pos(j,1) && b==bottom_pos(j,2)
+                        disp(ii,i) = disp_bottom_f(a,b,i);
+                    end
                 end
             end
-            for j=1:size(left_pos,1)
-                if a== left_pos(j,1) && b==left_pos(j,2)
-                    disp(ii,i) = disp_left_f(a,b,i);
+            if boundary_conditions(4+i)==0
+                for j=1:size(left_pos,1)
+                    if a== left_pos(j,1) && b==left_pos(j,2)
+                        disp(ii,i) = disp_left_f(a,b,i);
+                    end
                 end
             end
-            for j=1:size(right_pos,1)
-                if a== right_pos(j,1) && b==right_pos(j,2)
-                    disp(ii,i) = disp_right_f(a,b,i);
+            if boundary_conditions(6+i)==0
+                for j=1:size(right_pos,1)
+                    if a== right_pos(j,1) && b==right_pos(j,2)
+                        disp(ii,i) = disp_right_f(a,b,i);
+                    end
                 end
             end
         end
